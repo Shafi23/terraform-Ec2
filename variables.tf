39d736e6ac49eaab54c83711f54fdee57f64f594
Shafi <your-email@gmail.com>
2026-03-03 12:13:33 +0530

    Update Terraform state and variable descriptions for clarity

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "myami" {
-    default = "ami-019715e0d74f695be"
+    description = "Please enter your region specific ami"
 }
 
 variable "my_instance" {
-    default = "t3.micro"
+    description = "Please enter your instance type"
 }
 
 variable "My_region" {
-    default = "ap-south-1"
+    description = "Please enter your region"
 }
